427abd970f02f94217d2eb2c2a6f51a6605a4f98
felexmose <47243253+felexmose@users.noreply.github.com>
2019-05-08 13:27:09 +0200

    Update python.py

--- python.py
+++ python.py
@@ -1,14 +1,27 @@
+BROADCAST_TO_PORT = 7500
 from sense_hat import SenseHat
 import time
+from socket import *
+from datetime import datetime
 
 sense = SenseHat()
 sense.clear()
 
+s = socket(AF_INET, SOCK_DGRAM)
+#s.bind(('', 14593))     #(ip, port)
+# no explicit bind: will bind to default IP + random port
+s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 
 while True:
     pressure = sense.get_pressure()
-    pressure = round(pressure,1)
     print(pressure)
     sense.show_message(str(pressure))
+    #pressure = round(pressure,1)
+    trykEfterVaegt = pressure - 1002
+    VaegtIGram = 400/trykEfterVaegt
+      
+    data = "Current time " + str(datetime.now()) + "Weight " + str(VaegtIGram)
+	s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
+	print(data)
     time.sleep(5)
