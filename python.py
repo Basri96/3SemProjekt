53a74b37ec74a4844d1e60742c15b87dbf8a800b
zergcrafter <33547871+zergcrafter@users.noreply.github.com>
2019-05-09 12:31:09 +0200

    Update python.py

--- python.py
+++ python.py
@@ -12,19 +12,21 @@ s = socket(AF_INET, SOCK_DGRAM)
 # no explicit bind: will bind to default IP + random port
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-
+pressurecalibration = sense.get_pressure()
 while True:
     pressure = sense.get_pressure()
     print(round(pressure,1))
     sense.show_message(str(round(pressure,1)))
     #pressure = round(pressure,1)
-    trykEfterVaegt = (pressure - 996.8)
+    trykEfterVaegt = (pressure - pressurecalibration)
     print("tryk efter vaegt " + str(round(trykEfterVaegt,1)))
     vaegtIGram = round((trykEfterVaegt)*300)
     print("vaegt i gram " + str(round(vaegtIGram,1)))
+    now = datetime.now()
+    date = now.date()
     
       
-    data = "Current time " + str(datetime.now()) + "Weight " + str(vaegtIGram)
+    data = "Current time " + str(date) + "Weight " + str(vaegtIGram)
     s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
     print(data)
     time.sleep(1)
