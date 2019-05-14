896a5046a66319638d591e048f1d404f8c22fb07
zergcrafter <33547871+zergcrafter@users.noreply.github.com>
2019-05-14 13:24:07 +0200

    Update python.py

--- python.py
+++ python.py
@@ -13,7 +13,7 @@ s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 while True:
     pressurecalibration = sense.get_pressure()
-    if pressurecalibration != 0:
+    if pressurecalibration >= 950:
         break
 while True:
     pressure = sense.get_pressure()
