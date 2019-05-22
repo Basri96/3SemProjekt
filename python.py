15240376c0bcdee47a06a15d304295a5c72f0414
zergcrafter <33547871+zergcrafter@users.noreply.github.com>
2019-05-22 13:14:32 +0200

    Kommenteret noget af koden

--- python.py
+++ python.py
@@ -11,11 +11,16 @@ s = socket(AF_INET, SOCK_DGRAM)
 #s.bind(('', 14593))     #(ip, port)
 # no explicit bind: will bind to default IP + random port
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+#While loop for at kalibrer start trykket, da censoren ikke er tændt til at starte med
+#if delen er for at gøre den mere korrekt da den ikke måler det korrekte tryk når den starter
 while True:
     pressurecalibration = sense.get_pressure()
     if pressurecalibration >= 950:
         break
     time.sleep(5)
+    
+#Får pressure hver ~(10) sekund og printer det ud.
 while True:
     pressure = sense.get_pressure()
     print(round(pressure,1))
@@ -24,11 +29,12 @@ while True:
     trykEfterVaegt = (pressure - pressurecalibration)
     print("tryk efter vaegt " + str(round(trykEfterVaegt,1)))
     vaegtIGram = round((trykEfterVaegt)*300)
+    #Fordi baggrundstrykket varierer så når der ikke er noget ekstra vægt vil den give minus tryk, som så bliver sat til 0
     if vaegtIGram < 0:
         vaegtIGram = 0
     print("vaegt i gram " + str(round(vaegtIGram,1)))
     
-      
+    #Finder Current time and date, og kombinerer det med vægten fra før og broadcaster dataen, hvor recieveren opfanger det  
     data = "Current time " + str(datetime.now()) + " Weight " + str(vaegtIGram)
     s.sendto(bytes(data, "UTF-8"), ('<broadcast>', BROADCAST_TO_PORT))
     print(data)
